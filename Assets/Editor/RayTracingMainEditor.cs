ef29c155370d9ea29983585d45c7b7f306b79089
muramasa2402 <wongjengyan@gmail.com>
2019-10-28 15:01:51 +0000

    * Implemented progressive anti-aliasing * Added transparent shadows

---
+++ Assets/Editor/RayTracingMainEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(RayTracingMain))]
+    public class RayTracingMainEditor : UnityEditor.Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (!GUILayout.Button("Screenshot")) return;
+            ScreenCapture.CaptureScreenshot("/Users/bryanwong/Downloads/screenshot.png");
+//            var script = target as RayTracingMain;
+//            if (script == null) return;
+//            if (!script.Capture) script.Capture = true;
+        }
+    }
+}
--- Assets/Scripts/RayTracingMain.cs
+++ Assets/Scripts/RayTracingMain.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.IO;
+using UnityEngine;
+using Random = System.Random;
 
 public class RayTracingMain : MonoBehaviour
 {
@@ -7,20 +10,28 @@ public class RayTracingMain : MonoBehaviour
     public Texture skybox;
     
     private RenderTexture _target;
-
     private Camera _camera;
+    private uint _currentSample = 0;
+    private Material _addMaterial;
 
     private void Awake()
     {
         _camera = GetComponent<Camera>();
     }
 
+    private void Update()
+    {
+        if (!transform.hasChanged) return;
+        _currentSample = 0;
+        transform.hasChanged = false;
+    }
+    
     private void SetShaderParameters()
     {
         rtxShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
         rtxShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
     }
-    
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         SetShaderParameters();
@@ -35,12 +46,18 @@ public class RayTracingMain : MonoBehaviour
         // Set the target and dispatch the compute shader
         rtxShader.SetTexture(0, "Result", _target);
         rtxShader.SetTexture(0, "_skybox", skybox);
+        rtxShader.SetVector("_pixelOffset", UnityEngine.Random.insideUnitCircle);
         var threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
         var threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
         rtxShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
 
         // Blit the result texture to the screen
-        Graphics.Blit(_target, destination);
+        //Graphics.Blit(_target, destination);
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AAShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, destination, _addMaterial);
+        _currentSample++;
     }
 
     private void InitRenderTexture()
